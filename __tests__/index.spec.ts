2dffa6790850755d6143dbc3639459957cc60bc7
afeiship <1290657123@qq.com>
2024-08-10 13:35:24 +0800

    feat: target use default

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,10 +1,29 @@
+import fs from 'fs';
 import unzip from '../src';
 
 describe('api.basic', () => {
-  test('Normal single value case', async () => {
-    // await unzip({
-    //   source: '__tests__/dist.zip',
-    //   target: '__tests__/test123'
-    // });
+  beforeEach(() => {
+    // delete the __tests__/dist folder if it exists
+    fs.rmdirSync('__tests__/dist', { recursive: true });
+    fs.rmdirSync('__tests__/test124', { recursive: true });
   });
+
+  test('No target will use default target', async () => {
+    await unzip({
+      source: '__tests__/dist.zip',
+    });
+
+    expect(fs.existsSync('__tests__/dist/index.d.ts')).toBe(true);
+  });
+
+  test('has target will use target', async ()=>{
+    await unzip({
+      source: '__tests__/dist.zip',
+      target: '__tests__/test124',
+      zlOptions: {
+        overwrite: true
+      }
+    });
+    expect(fs.existsSync('__tests__/test124/dist/index.d.ts')).toBe(true);
+  })
 });
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,22 @@
 import { Unzip, IExtractOptions } from 'zip-lib';
+import path from 'path';
 
 interface UnzipOptions {
   source: string;
-  target: string;
+  target?: string;
   onEntry?: IExtractOptions['onEntry'];
   zlOptions?: Omit<IExtractOptions, 'onEntry'>;
 }
 
 const unzip = (inOptions: UnzipOptions) => {
   const { source, target, onEntry, zlOptions } = inOptions;
+  const _target = target || path.dirname(source);
   const unzip = new Unzip({
     onEntry,
     ...zlOptions,
   });
 
-  return unzip.extract(source, target);
+  return unzip.extract(source, _target);
 };
 
 export default unzip;
